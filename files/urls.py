50901d426b161bb634a3c4b20a3e9c052803cc53
Abbos Zoirov <122oszoirov@gmail.com>
2024-07-24 22:10:33 +0500

    add views

--- files/urls.py
+++ files/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('upload/', views.file_upload_view, name='upload'),
+    path('list/', views.file_list_view, name='list'),
+    path('download/<int:pk>/', views.file_download_view, name='download'),
 ]
--- files/views.py
+++ files/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from .models import File
+from .serializers import FileSerializer
 
-# Create your views here.
+@api_view(['POST'])
+@parser_classes([MultiPartParser, FormParser])
+def file_upload_view(request):
+    if request.method == 'POST':
+        serializer = FileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET'])
+def file_list_view(request):
+    if request.method == 'GET':
+        files = File.objects.all()
+        serializer = FileSerializer(files, many=True)
+        return Response(serializer.data)
+    
+@api_view(['GET'])
+def file_download_view(request, pk):
+    file = get_object_or_404(File, pk=pk)
+    response = HttpResponse(file.file, content_type='application/octet-stream')
+    response['Content-Disposition'] = f'attachment; filename="{file.file.name}"'
+    return response
